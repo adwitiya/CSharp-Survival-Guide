81193cda18e9fd14f9dcd29bf9a107f782595d98
adwitiya <chakraad@tcd.ie>
2020-03-22 23:08:07 +0000

    Intial Commit

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public Vector3 startPos;
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.transform.position = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
